7c198c334915f95b1dd79e4b50f859a179f66da3
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-01-19 22:49:06 +0200

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DataTypeTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int meters = int.Parse(Console.ReadLine());  //we read the input in meters
+            float kilometers = meters / 1000.0F;
+            Console.WriteLine($"{kilometers:F2}");
+
+        }
+    }
+}
